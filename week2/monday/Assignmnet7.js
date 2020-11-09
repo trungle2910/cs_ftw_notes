502b4f6afa2c2b8690524954a2e2a2f0914a7456
TrungLe <thanhtrungle291097@gmail.com>
2020-11-09 22:24:54 +0700

    Monday homeworks

--- week2/monday/Assignmnet7.js
+++ week2/monday/Assignmnet7.js
@@ -1,7 +1,36 @@
 function outputRange(start, end){
     var result="";
-    for (start ; start < end; start++ ){
-        result = result + " " + start
+    for (start ; start <= end; start++ ){
+        convertResult = convertNumber(start);
+        result = result  + convertResult + ", "
+  }
+  console.log("output: " + result);
+}
+
+outputRange(1,50);
+function convertNumber(number){
+  if( number % 3 === 0){
+    return "Fizz";
+  }
+  else if ( number % 5 === 0){
+    return "Buzz";
+  }
+  else if ( number % 7 === 0){
+    return "Super";
+  }
+  else if ( number % 3 === 0 && number % 5 === 0){ 
+    return "FizzBuzz";
+  }
+  else if ( number % 5 === 0 && number % 7 === 0){
+    return "SuperBuzz";
+  }
+  else if ( number % 3 === 0 && number % 7 === 0){
+    return "SuperFizz";
+  }
+  else if ( number % 3 === 0 && number % 5 === 0 && number % 7 === 0){
+    return "SuperFizzBuzz";
+  }
+  else{
+    return number;
   }
 }
-outputRange(1,50);
